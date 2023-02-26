0cc7e55bddc79500a608f425e461f555785bad09
PetroYurchuk <petroyurchuk66@gmail.com>
2023-02-26 22:07:19 +0200

    add component main

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,12 @@
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
+import Main from '../Main/Main'
 import './App.css'
 function App() {
     return (
         <div className="container">
             <Header />
+            <Main />
             <Footer />
         </div>
     )
---
+++ src/Main/Main.tsx
@@ -0,0 +1,11 @@
+import './Main.css'
+
+function Main() {
+    return (
+        <div className="main">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque,
+            ipsa.
+        </div>
+    )
+}
+export default Main
