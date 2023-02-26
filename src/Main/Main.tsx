c9aa9242bcca00c6d38e8fdef38af425e37b9a39
PetroYurchuk <petroyurchuk66@gmail.com>
2023-02-26 22:30:22 +0200

    add titles and articles for main component

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,11 +1,14 @@
 import './Main.css'
 import articles from '../data/articles.json'
-console.log(articles)
 function Main() {
     return (
         <div className="main">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque,
-            ipsa.
+            {articles.map((item) => (
+                <section>
+                    <h2>{item.title}</h2>
+                    <div>{item.body}</div>
+                </section>
+            ))}
         </div>
     )
 }
